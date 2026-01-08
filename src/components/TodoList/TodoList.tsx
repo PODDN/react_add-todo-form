03494fbee5f8e36218ec7aea1439157ccaa9b211
Denys Piddubnyi <iforgotmypincode@gmail.com>
2026-01-08 12:16:52 +0200

    feat: implement NewMovie form with validation and reset logic

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,16 @@
-export const TodoList = () => {};
+import { Todos } from '../../types/Todos';
+import { TodoInfo } from '../TodoInfo';
+
+type TodoListProps = {
+  todos: Todos[];
+};
+
+export const TodoList = ({ todos }: TodoListProps) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  );
+};
